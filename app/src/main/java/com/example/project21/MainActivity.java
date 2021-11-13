70a38aa0f3bae3f4f14ff04bd317428128bdc391
tcmille9 <93411010+tcmille9@users.noreply.github.com>
2021-11-12 21:01:05 -0700

    Added a whole lot

---
+++ app/src/main/java/com/example/project21/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.project21;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+Button but;
+    FragmentManager fm;
+    MainActivityFragment fragment;
+    int i = 0;
+    TextView tv = null;
+    int a = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        fm = getSupportFragmentManager();
+        fragment = (MainActivityFragment)fm.findFragmentById(R.id.fragment);
+        tv = fragment.tv;
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate menu and add items to action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void clickMethod(View view) {
+        but.setText("changed");
+        FragmentManager fm = getSupportFragmentManager();
+        // MainActivityFragment fragment = (MainActivityFragment)fm.findFragmentById(R.id.fragment);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
